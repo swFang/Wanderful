8b69c96fe578fbfbce82a3be2ac1ce8d66cf2124
Grace Luong <grace-luong@hotmail.com>
2020-01-12 03:08:17 -0800

    added itinerary components

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import MapContainer from './MapContainer';
 import SimpleCard from './components/travelAdvisoryBoard';
 import Profile from './components/profile';
+import Itinerary from './components/itinerary';
 
 function App() {
   return (
     <div className="App">
+      <Itinerary/>
       <Profile/>
       <SimpleCard/>
       <MapContainer></MapContainer>
