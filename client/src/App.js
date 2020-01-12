6405fb2df1871540c2058b22803a0c560cb3d992
j0i1b <j0i1b@ugrad.cs.ubc.ca>
2020-01-11 20:01:45 -0800

    added in shell simple card

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import MapContainer from './MapContainer';
+import SimpleCard from './components/travelAdvisoryBoard';
 
 function App() {
   return (
     <div className="App">
+      <SimpleCard/>
       <MapContainer></MapContainer>
     </div>
   );
